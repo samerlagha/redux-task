efa692f19b31745091467b432d3f1e3038af7c1e
samerlagha <samer_la@mail.ru>
2020-05-11 14:15:32 +0300

    commit

---
+++ lesson1/hw/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Header from "./Header";
+import {  UserDataContext } from "./userData-context";
+
+class App extends Component {
+  state = {
+    userData: {
+      name: "Nikola Tesla",
+      avatar_url: "https://avatars3.githubusercontent.com/u10001",
+    },
+  };
+  render() {
+    return (
+      <div className="page">
+        <UserDataContext.Provider value={this.state.userData}>
+          <Header />
+        </UserDataContext.Provider>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ lesson1/hw/src/UserMenu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import UserData from "./UserData";
+
+
+class UserMenu extends React.Component {
+  render() {
+    return <UserData />;
+  }
+}
+
+export default UserMenu;
