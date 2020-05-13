c176e835a30de534d04f0561f25ece6b16d1f094
samerlagha <samer_la@mail.ru>
2020-05-13 13:39:57 +0300

    commit

---
+++ lesson3/task1/src/index.js
@@ -0,0 +1,11 @@
+import store from './store';
+import {increment , decrement ,reset}  from './counter.actions';
+
+import { addUser, updateUser, deleteUser } from './users.actions';
+
+store.subscribe(()=>console.log(store.getState()));
+
+store.dispatch(addUser({id: 21, name: 'James'}));
+store.dispatch(addUser({id: 20, name: 'Tom'}));
+store.dispatch(updateUser(21, {age: 22}));
+store.dispatch(deleteUser(20));
