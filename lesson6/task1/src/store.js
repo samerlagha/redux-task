975833e45f0be5389a3a15be861f36274172fc78
samerlagha <samer_la@mail.ru>
2020-05-27 16:26:20 +0300

    commit

--- lesson6/task1/src/store.js
+++ lesson6/task1/src/store.js
@@ -1,16 +1,20 @@
-import { createStore, applyMiddleware,compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import counterReducer from './counter.reducer';
-const logger =store =>next=>action=>{
+
+const logger = store => next => action => {
     console.group(action.type);
-    console.info('dispatching',action);
+    console.info('dispatching', action);
     let result = next(action);
-    console.log('nexr state', store.getState());
+    console.log('next state', store.getState());
     console.groupEnd();
-    return result();
+    return result;
 };
-const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store=  createStore(
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
     counterReducer,
     composeEnhancers(applyMiddleware(logger))
 );
+
 export default store;
